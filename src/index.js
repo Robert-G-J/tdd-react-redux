fc0d77a4f7d3e6e25704a34385425f1b90fb90a9
Robert Jones <robert.g.jones@gmail.com>
2018-02-07 15:42:58 +0000

    Can store a todo in state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/* global document */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from "redux";
+import todoListApp from "./reducers/";
+
+const reducers = combineReducers({
+  todoListApp
+});
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
